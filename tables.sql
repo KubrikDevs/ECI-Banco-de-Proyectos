f93d1f8ec16c9747fd073e42616dc587f989d74b
Juan Camilo <juaco9602@hotmail.com>
2020-04-13 21:00:42 -0500

    Correcciónes front

--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Usuarios (
+/*CREATE TABLE Usuarios (
     nombre varchar(50)  NOT NULL,
     correo varchar(150)  NOT NULL,
     contraseña varchar(50)  NOT NULL,
@@ -6,3 +6,21 @@ CREATE TABLE Usuarios (
     rol varchar(15)  NOT NULL,
     CONSTRAINT Usuarios_pk PRIMARY KEY (correo)
 );
+
+CREATE TABLE Iniciativa (
+    id int  NOT NULL,
+    nombre varchar(150)  NOT NULL,
+    estado varchar(50)  NOT NULL,
+    numVotos int  NOT NULL,
+    usuarioProponente varchar(150),
+    area varchar(50)  NOT NULL,
+    fechaCreacion date  NOT NULL,
+    fechaFin date  NOT NULL,
+    descripcion String(150),
+    CONSTRAINT Iniciativa_pk PRIMARY KEY (id)
+);
+
+ALTER TABLE Iniciativa ADD CONSTRAINT Iniciativa_usuario
+    FOREIGN KEY (usuarioProponente)
+    REFERENCES Usuarios(correo);
+*/
